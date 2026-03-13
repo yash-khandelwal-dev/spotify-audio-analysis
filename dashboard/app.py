730ff5f54ffd46932acee3e08ac0d132620d8738
Yash khandelwal <yashkhandelwal0409@gmail.com>
2026-03-13 23:39:21 +0530

    fixed column name issue

--- dashboard/app.py
+++ dashboard/app.py
@@ -28,6 +28,7 @@ st.markdown("Interactive exploration of Spotify audio features and song populari
 def load_data():
     url = "https://drive.google.com/uc?id=1Wr3S8Wfwk8otcuNagSPWRDBFHgR5xdTL"
     df = pd.read_csv(url)
+    df.columns = df.columns.str.strip().str.lower()
     return df
 
 df = load_data()
